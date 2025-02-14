97e150b332e5f4124ec5aac019cbcd299dca8e7a
shenarie nicole <MagsayoShenarieNicole_Bsit@plmun.edu.ph>
2025-02-15 03:32:33 +0800

    Submitting Python Week 04 - Laboratory # 09

---
+++ if_else_statement.py
@@ -0,0 +1,16 @@
+# Magsayo Shenarie Nicole
+# ITELEC2
+
+def main():
+    user_input = input("Enter a number: ")
+    number = int(user_input)
+    if number % 2 == 0:
+    print("The number", number, "is Even.")
+    else:
+    print("The number", number, "is Odd.")
+    try:
+    #code here
+    except ValueError:
+    print("Invalid input. Please enter an integer.")
+if __name__ == "__main__":
+    main()
